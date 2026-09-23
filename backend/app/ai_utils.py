0f1f8055412c3f5c953ad538c1dee396e220ba08
DIXKOX <dixkoxinc@gmail.com>
2026-09-23 14:00:03 -0400

    Remove external AI dependency

--- backend/app/ai_utils.py
+++ backend/app/ai_utils.py
@@ -1,17 +1,11 @@
-import os
-from huggingface_hub import InferenceClient
-
-client = InferenceClient(
-    token=os.getenv("HF_TOKEN")
-)
-
 def ask_gemini(question: str, text: str) -> str:
         try:
+            question = question.lower()
 
-            sentences = text.split(".")
-            for sentence in sentences:
-                if "vacation" in sentence.lower():
-                    return sentence.strip()
+            if "vacation" in question:
+                for line in text.split("."):
+                 if "vacation" in line.lower():
+                     return line.strip()
 
             return text[:500]
 
